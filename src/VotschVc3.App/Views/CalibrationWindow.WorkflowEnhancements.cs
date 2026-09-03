1d8924ccbcb241ee969613603af1cf81425ed8cb
Marek Múčka <mukky89@gmail.com>
2026-09-03 14:50:54 +0200

    Fix profile type import in calibration workflow enhancements

--- src/VotschVc3.App/Views/CalibrationWindow.WorkflowEnhancements.cs
+++ src/VotschVc3.App/Views/CalibrationWindow.WorkflowEnhancements.cs
@@ -6,6 +6,7 @@ using System.Windows.Media;
 using System.Windows.Threading;
 using VotschVc3.App.ViewModels;
 using VotschVc3.Core.Diagnostics;
+using VotschVc3.Core.Profiles;
 
 namespace VotschVc3.App.Views;
 
